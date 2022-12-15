13e103ed525b5d938b043afccae81c47c8f8d69a
Documenter.jl <documenter@juliadocs.github.io>
2022-12-15 15:06:59 +0000

    build based on 6e006d1

--- dev/search_index.js
+++ dev/search_index.js
@@ -1,3 +1,3 @@
 var documenterSearchIndex = {"docs":
-[{"location":"#github_doc.jl","page":"github_doc.jl","title":"github_doc.jl","text":"","category":"section"},{"location":"","page":"github_doc.jl","title":"github_doc.jl","text":"Documentation for github_doc.jl","category":"page"},{"location":"","page":"github_doc.jl","title":"github_doc.jl","text":"This package was built to practice using CI and to deploy documentation in Github for a julia package.","category":"page"}]
+[{"location":"#github_doc.jl","page":"Introduction","title":"github_doc.jl","text":"","category":"section"},{"location":"","page":"Introduction","title":"Introduction","text":"Documentation for github_doc.jl","category":"page"},{"location":"","page":"Introduction","title":"Introduction","text":"This package was built to practice using CI and to deploy documentation in Github for a julia package.","category":"page"}]
 }
