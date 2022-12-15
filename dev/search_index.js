9d5bec143e65e4fc6cfd8bb5980e8ffc6fb5cbe9
Documenter.jl <documenter@juliadocs.github.io>
2022-12-15 14:06:38 +0000

    build based on adc4027

---
+++ dev/search_index.js
@@ -0,0 +1,3 @@
+var documenterSearchIndex = {"docs":
+[{"location":"#github_doc.jl","page":"github_doc.jl","title":"github_doc.jl","text":"","category":"section"},{"location":"","page":"github_doc.jl","title":"github_doc.jl","text":"Documentation for github_doc.jl","category":"page"}]
+}
