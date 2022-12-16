682856448ba0556a570591a7036c30abb6d94834
Documenter.jl <documenter@juliadocs.github.io>
2022-12-16 11:56:32 +0000

    build based on 00f930b

---
+++ 0.1.0/search_index.js
@@ -0,0 +1,3 @@
+var documenterSearchIndex = {"docs":
+[{"location":"#github_doc.jl","page":"Introduction","title":"github_doc.jl","text":"","category":"section"},{"location":"","page":"Introduction","title":"Introduction","text":"Documentation for github_doc.jl","category":"page"},{"location":"","page":"Introduction","title":"Introduction","text":"This package was built to practice using CI and to deploy documentation in Github for a julia package.","category":"page"}]
+}
